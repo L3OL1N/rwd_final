81cd855757fe06ecaa8e893a6ae88da25561c891
L3OL1N <107172548+L3OL1N@users.noreply.github.com>
2022-07-10 10:01:04 +0800

    Add files via upload
    +img alt description
    -light box2

--- js/all.js
+++ js/all.js
@@ -4,40 +4,6 @@ $(document).ready(function () {
     event.preventDefault();
     $(this).siblings("ul").toggleClass("show");
   });
-  //下拉式選單
-  $('.dropdown').click(function(event) {
-    /* Act on the event */
-    event.preventDefault();
-    $(this).toggleClass('active').parent().siblings().find('.dropdown').removeClass('active');
-    $(this).parent().find('.dropdown-open').slideToggle();
-    $(this).parent().siblings().find('.dropdown-open').slideUp();
-  });
-
-  //燈箱
-  const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    // this shows a bit of the previous/next slides
-    slidesPerView: 1,
-    centeredSlides: true,
-    spaceBetween: 20,
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
 
   //top
   $('.toTop-arrow').click(function (event) { 
